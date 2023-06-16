8e944b01dc476a8419db5cefe6dacd18ce69b8f2
Michael Dippold <michael@thebrickfactory.com>
2023-06-15 19:35:22 -0500

    wrapping up basic work

--- index.js
+++ index.js
@@ -1,105 +1,210 @@
-// document.addEventListener("DOMContentLoaded", function () {
 
-    function initMap() {
+function initMap() {
 
-        var mapStyle = [
-            {
-                featureType: 'poi',
-                stylers: [
-                    { visibility: 'off' }
-                ]
-            }
-        ];
-        
-        map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 13.8,
-          center: new google.maps.LatLng(41.717354, -93.600563),
-          mapTypeId: "roadmap",
-          styles: mapStyle,
-          isFractionalZoomEnabled: true,
-          streetViewControl: false,
-          mapTypeControl: false
-        });
-      
-        const iconBase = "img/";
-        const icons = {
-          on_with_life: {
-            name: "On With Life",
-            icon: iconBase + "owl.svg",
-            sizeW: 60,
-            sizeH: 60,
-          },
-          hyvee: {
-            name: "HyVee",
-            icon: iconBase + "hyvee.svg",
-            sizeW: 70,
-            sizeH: 40,
-          },
-          fareway: {
-            name: "Fareway",
-            icon: iconBase + "fareway.svg",
-            sizeW: 80,
-            sizeH: 40,
-          },
-          walmart: {
-            name: "Walmart",
-            icon: iconBase + "walmart.svg",
-            sizeW: 100,
-            sizeH: 50,
-          },
-        };
-        const features = [
-          {
-            position: new google.maps.LatLng( 41.698770, -93.608780 ),
-            type: "on_with_life",
-          },
-          {
-            position: new google.maps.LatLng( 41.704891, -93.623169 ),
-            type: "hyvee",
-          },
-          {
-            position: new google.maps.LatLng( 41.734640, -93.602260 ),
-            type: "hyvee",
-          },
-          {
-            position: new google.maps.LatLng( 41.702110, -93.599690 ),
-            type: "fareway",
-          },
-          {
-            position: new google.maps.LatLng( 41.704781, -93.584122 ),
-            type: "walmart",
-          },
-        ];
-      
-        features.forEach((feature) => {
-            console.log(icons[feature.type].icon);
-          new google.maps.Marker({
-            position: feature.position,
-            icon: {
-                url: icons[feature.type].icon,
-                scaledSize: new google.maps.Size( icons[feature.type].sizeW, icons[feature.type].sizeH ),
-            },
-            map: map,
-            
-          });
-        });
-      
-        const legend = document.getElementById("legend");
-      
-        for (const key in icons) {
-            const type = icons[key];
-            const name = type.name;
-            const icon = type.icon;
-            const height = type.sizeH;
-            const width = type.sizeW;
-            const div = document.createElement("div");
-      
-            div.innerHTML = '<div class="legend-row"><img src="' + icon + '" style="height:' + height + 'px;width:' + width + 'px;"><div>' + name + '</div></div>';
-            legend.appendChild(div);
+    urlencode = function (str) {
+        str = (str + '').toString();
+        return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').
+        replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+');
+    };
+
+    const center = new google.maps.LatLng(41.717427, -93.592745)
+
+    var mapStyle = [
+        {
+            featureType: 'poi',
+            stylers: [
+                { visibility: 'off' }
+            ]
         }
-      
-        //map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
-      }
-      
-      window.initMap = initMap;
-// });
+    ];
+    
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14.4,
+        center: center,
+        mapTypeId: "roadmap",
+        styles: mapStyle,
+        isFractionalZoomEnabled: true,
+        streetViewControl: false,
+        mapTypeControl: false
+    });
+    
+    const iconBase = "img/";
+    const icons = {
+        on_with_life: {
+        name: "On With Life",
+        icon: iconBase + "owl.svg",
+        sizeW: 60,
+        sizeH: 60,
+        },
+        hyvee: {
+        name: "HyVee",
+        icon: iconBase + "hyvee.svg",
+        sizeW: 70,
+        sizeH: 40,
+        },
+        fareway: {
+        name: "Fareway",
+        icon: iconBase + "fareway.svg",
+        sizeW: 80,
+        sizeH: 40,
+        },
+        walmart: {
+        name: "Walmart",
+        icon: iconBase + "walmart.svg",
+        sizeW: 100,
+        sizeH: 50,
+        },
+        postoffice: {
+        name: "Post Office",
+        icon: iconBase + "usps.png",
+        sizeW: 60,
+        sizeH: 50,
+        },
+        homewoodsuites: {
+        name: "Homewood Suites",
+        icon: iconBase + "homewood.svg",
+        sizeW: 100,
+        sizeH: 60,
+        },
+        residenceinn: {
+        name: "Residence Inn",
+        icon: iconBase + "residenceinn.png",
+        sizeW: 100,
+        sizeH: 60,
+        },
+        caseys: {
+        name: "Casey's",
+        icon: iconBase + "caseys.svg",
+        sizeW: 60,
+        sizeH: 40,
+        },
+        target: {
+        name: "Target",
+        icon: iconBase + "target.svg",
+        sizeW: 40,
+        sizeH: 40,
+        },
+        kumgo: {
+        name: "Kum & Go",
+        icon: iconBase + "kumgo.png",
+        sizeW: 60,
+        sizeH: 40,
+        },
+    };
+    const features = [
+        {
+        position: new google.maps.LatLng( 41.698770, -93.608780 ),
+        type: "on_with_life",
+        address: "715 SW Ankeny Rd, Ankeny, IA 50023",
+        },
+        {
+        position: new google.maps.LatLng( 41.704891, -93.623169 ),
+        type: "hyvee",
+        address: "410 N Ankeny Blvd, Ankeny, IA 50023",
+        },
+        {
+        position: new google.maps.LatLng( 41.734640, -93.602260 ),
+        type: "hyvee",
+        address: "2510 SW State St, Ankeny, IA 50023",
+        },
+        {
+        position: new google.maps.LatLng( 41.702110, -93.599690 ),
+        type: "fareway",
+        address: "1202 N Ankeny Blvd, Ankeny, IA 50023",
+        },
+        {
+        position: new google.maps.LatLng( 41.704781, -93.584122 ),
+        type: "walmart",
+        address: "1002 SE National Dr, Ankeny, IA 50021",
+        },
+        {
+        position: new google.maps.LatLng( 41.720670, -93.600300 ),
+        type: "postoffice",
+        address: "1011 N Ankeny Blvd, Ankeny, IA 50023",
+        },
+        {
+        position: new google.maps.LatLng( 41.704418, -93.569763 ),
+        type: "homewoodsuites",
+        address: "2455 SE Creekview Dr, Ankeny, IA 50021",
+        },
+        {
+        position: new google.maps.LatLng( 41.713140, -93.616910 ),
+        type: "residenceinn",
+        address: "1515 SW Main St, Ankeny, IA 50023",
+        },
+        {
+        position: new google.maps.LatLng( 41.705090, -93.582180 ),
+        type: "caseys",
+        address: "1102 E 1st St, Ankeny, IA 50021",
+        },
+        {
+        position: new google.maps.LatLng( 41.729340, -93.600960 ),
+        type: "caseys",
+        address: "2402 W 1st St, Ankeny, IA 50023",
+        },
+        {
+        position: new google.maps.LatLng( 41.703750, -93.571120 ),
+        type: "caseys",
+        address: "2402 SE Delaware Ave, Ankeny, IA 50021",
+        },
+        {
+        position: new google.maps.LatLng( 41.709380, -93.578550 ),
+        type: "target",
+        address: "2135 SE Delaware Ave, Ankeny, IA 50021",
+        },
+    ];
+
+    const infowindow = new google.maps.InfoWindow({
+        content: "",
+    });
+    
+    features.forEach((feature) => {
+        const marker = new google.maps.Marker({
+        position: feature.position,
+        icon: {
+            url: icons[feature.type].icon,
+            scaledSize: new google.maps.Size( icons[feature.type].sizeW, icons[feature.type].sizeH ),
+        },
+        map: map,
+        });
+        marker.addListener("click", () => {
+        map.setZoom(17);
+        // map.setCenter(marker.getPosition());
+        map.panTo(marker.getPosition());
+        infowindow.setPosition(marker.getPosition());
+        infowindow.open({
+            anchor: marker,
+            map,
+        });
+        infowindow.setContent(
+            '<h2>' + icons[feature.type].name + '</h2>' + 
+            '<a  target="_blank" href="https://www.google.com/maps/search/?api=1&query=' + urlencode(feature.address)  + '">' + feature.address + '</a>'
+        );
+        
+
+        });
+    
+    });
+    
+    const legend = document.getElementById("legend");
+    
+    for (const key in icons) {
+        const type = icons[key];
+        const name = type.name;
+        const icon = type.icon;
+        const height = type.sizeH;
+        const width = type.sizeW;
+        const div = document.createElement("div");
+    
+        div.innerHTML = '<div class="legend-row"><img src="' + icon + '" style="height:' + height + 'px;width:' + width + 'px;"><div>' + name + '</div></div>';
+        legend.appendChild(div);
+    }        
+    
+    document.getElementById("reset-button").addEventListener("click", () => {
+        map.setZoom(14.4);
+        map.setCenter(center);
+        infowindow.close();
+    });
+}
+
+window.initMap = initMap;
